b2206d2443b56f651a50f0f2bce756c0a3c72420
Aakash <saiaakash007@gmai.com>
2025-07-22 12:44:14 +0530

    final changes to logger module to log in logrecords.json

--- Fullstack/url-shortener/evaluation-service/logs.jsx
+++ Fullstack/url-shortener/evaluation-service/logs.jsx
@@ -1,5 +1,8 @@
+const fs=require('fs');
 function Log (stack,level,pkg,message) {
-    window.message(`${stack} throws ${level} warning in ${pkg} package with message ${message}`)
-}
+    const data=`${stack} throws ${level} warning in ${pkg} package with message ${message}`;
+    const jsonData=JSON.stringify(data,null,2);
+    fs.writeFile('logrecords.json',jsonData);
+    }
 
 export default Log
